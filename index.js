858deff6dd8cc73f48e8a8ed728f2128f2914254
Michael Douglas <140912426+MicDev00@users.noreply.github.com>
2023-09-30 23:18:19 -0300

    Desafio Criado

---
+++ index.js
@@ -0,0 +1,31 @@
+// Desafio Classificador de nível de Herói
+
+let heroi = "Batman"
+let xp = 1.001
+
+if (xp <= 1.000){
+   console.log("Ferro")
+}
+else if (xp == 1.001 || xp == 2.000){
+    console.log("Bronze")
+}
+else if (xp == 2.001 || xp == 5.000){
+    console.log("Prata")
+}
+else if (xp == 6.001 || xp == 7.000){
+    console.log("Ouro")
+}
+else if (xp == 7.001 || xp == 8.000){
+    console.log("Platina")
+}
+else if (xp == 8.001 || xp == 9.000){
+    console.log("Ascendente")
+}
+else if (xp == 9.001 || xp == 10.000){
+    console.log("Imortal")
+}
+else if (xp >= 10.001){
+    console.log("Radiante")
+}
+
+console.log("O Herói cujo nome é " + heroi +" está no nível "+ xp)
